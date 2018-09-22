8157d51c4a16ada613f4066145d03f5fa81c3711
Dr. Neil Brittliff <Neil.Brittliff@iinet.net.au>
2018-09-23 09:51:23 +1000

    First Commit

---
+++ main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+//handle setupevents as quickly as possible
+const setupEvents = require('./installers/setupEvents');
+
+if (setupEvents.handleSquirrelEvent()) {
+   return;
+}
+
+const electron = require('electron');
+const {app, protocol} = require('electron');
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path')
+const url = require('url')
+
+var mainWindow = null;
+
+
+function createWindow () {
+
+  mainWindow = new BrowserWindow({width: 980, height: 644, resizable: false, autoHideMenuBar: true})
+  mainWindow.setMenu(null);
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+
+}
+ 
+app.on('ready', createWindow)
+
+app.on('window-all-closed',  () => {
+  app.quit()
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
