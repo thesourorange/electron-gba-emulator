0b3e82b5a2230b2da637e6032e79e7ae2dd732b2
Dr. Neil Brittliff <Neil.Brittliff@iinet.net.au>
2019-06-18 22:52:34 +1000

    Updated Code Base

--- main.js
+++ main.js
@@ -16,10 +16,9 @@ const url = require('url')
 
 var mainWindow = null;
 
-
 function createWindow () {
 
-  mainWindow = new BrowserWindow({width: 1235, height: 720, resizable: false, autoHideMenuBar: true})
+  mainWindow = new BrowserWindow({width: 980, height: 580, resizable: false, autoHideMenuBar: true})
   mainWindow.setMenu(null);
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
